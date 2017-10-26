13bea29ba2cb26bf653ea904c624552e350328d9
Nico Rehwaldt <git_nikku@nixis.de>
2017-10-26 08:54:42 +0200

    chore(tests): split up by aspect

---
+++ test/errors.js
@@ -0,0 +1,83 @@
+var EasySax = require('../easysax');
+var assert = require('assert');
+
+
+describe('handler errors', function() {
+
+    var parser;
+
+    beforeEach(function() {
+
+        parser = new EasySax();
+
+        parser.ns('foo', {
+            'http://foo': 'foo'
+        });
+    })
+
+    it('should pass to #onError', function() {
+
+        // given
+        var called = false;
+
+        parser.on('error', function(err, getContext) {
+
+            called = true;
+
+            assert.equal(err.message, 'foo');
+            assert.ok(getContext);
+        });
+
+        parser.on('startNode', function() {
+            throw new Error('foo');
+        });
+
+        // when
+        parser.parse('<xml />');
+
+        // then
+        assert.ok(called);
+    });
+
+
+    it('should handle in #onError', function() {
+
+        // given
+        var called = false;
+
+        parser.on('error', function(err, getContext) {
+            throw err;
+        });
+
+        parser.on('startNode', function() {
+            throw new Error('foo');
+        });
+
+        // when
+        assert.throws(function() {
+            parser.parse('<xml />');
+        }, /foo/);
+    });
+
+});
+
+
+describe('ns configuration', function() {
+
+    it('should throw on missing default ns uri', function() {
+
+        // given
+        var parser = new EasySax();
+
+        // when
+        function configure() {
+            parser.ns('bar', {
+                'http://foo': 'foo'
+            });
+        }
+
+        // then
+        assert.throws(configure, /no namespace uri defined for <bar>/);
+    });
+
+});
---
+++ test/test.js
@@ -0,0 +1,20 @@
+/**
+ * Test suite main.
+ */
+
+describe('document parsing', function() {
+
+  describe('should parse', function() {
+
+    require('./elements');
+
+  });
+
+});
+
+
+describe('errors', function() {
+
+  require('./errors');
+
+});
