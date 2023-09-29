143b389baadb8e04441c6566f5793b3138a39d2a
Nico Rehwaldt <git_nikku@nixis.de>
2023-09-29 08:30:50 +0200

    chore: drop pre-built UMD distribution
    Users can consume the ESM build in modern browsers just fine.

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,6 @@
-import terser from '@rollup/plugin-terser';
-
 import pkg from './package.json';
 
 export default [
-
-  // browser-friendly UMD build
-  {
-    input: 'lib/index.js',
-    output: {
-      name: 'Saxen',
-      file: 'dist/saxen.umd.js',
-      format: 'umd'
-    }
-  },
-  {
-    input: 'lib/index.js',
-    output: {
-      name: 'Saxen',
-      file: 'dist/saxen.umd.min.js',
-      format: 'umd'
-    },
-    plugins: [
-      terser()
-    ]
-  },
   {
     input: 'lib/index.js',
     output: [
